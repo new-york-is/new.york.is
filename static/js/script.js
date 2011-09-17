493ecbdb30f0f7d01791934609187b6c95451d3e
John Paul <john@johnkpaul.com>
2011-09-17 18:17:03 -0400

    begin css3 resizing of rows

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,23 @@ var Event = Backbone.Model.extend({
         }
     });
 
+var EventCollection = Backbone.Collection.extend({
+        model: Event,
+        initialize:function(){
+            _.bindAll(this,"removeEvent");
+        },
+        add:function(event){
+            Backbone.Collection.prototype.add.call(this, event);
+            event.bind("animationFinished", this.removeEvent);
+            return this;
+        },
+        removeEvent:function(event){
+            this.remove(event);
+        }
+    }); 
+
 var EventView = Backbone.View.extend({
+        tagName:"li",
         className:"event",
         initialize:function(){
             _.bindAll(this,"render");
@@ -12,34 +28,60 @@ var EventView = Backbone.View.extend({
             this.template = _.template($("#event-view").html());
         },
         render:function(){
-            var getDropHeight = function(){
-                return $(window).height()+el.height();
-            };
-            var getRandomInRangeOfHeight = function(){
-                Math.floor(Math.random() * $(window).height()+$(this.el).height());
-            }
-
-
-            var el = $(this.el);
+            var el = $(this.el),
+                self = this;
             el.html(this.template(this.model.toJSON()));
+
             el.css("left",$(window).width());
-            var randomHeightInViewport = getRandomInRangeOfHeight();
-            el.css("top",$(window).height()-randomHeightInViewport);
-            el.animate({"left":-2*$(this.el).width()},2000,function(){
-                    el.animate({"top":"+="+getDropHeight()}, 2000);
+            el.css("top",100);
+            
+            var elWidth = this.getElWidth();
+            el.animate({"left":-elWidth},4000,"easeInOutSine", function(){
+               self.model.trigger("animationFinished");
             });
 
             return this;
+        },
+        getElWidth:function(){
+            var newEl = this.el.cloneNode(true);
+            newEl.style.visibility="hidden";
+            document.body.appendChild(newEl);
+            var width = $(newEl).width();
+            newEl.parentNode.removeChild(newEl);
+            return width;
         }
 });
+
+var RowView = Backbone.View.extend({
+        tagName:"ul",
+        initialize:function(){
+            _.bindAll(this,"render","renderEvent");
+            this.template = _.template($("#row-view").html());
+            this.collection.bind("add",this.renderEvent);
+        },
+        render:function(){
+            $(this.el).html(this.template()); 
+            this.collection.each(this.renderEvent); 
+            return this;
+        },
+        renderEvent:function(event){
+           var eventView = new EventView({
+                    model:event
+            }); 
+            $(this.el).append(eventView.render().el);
+        }
+    });
 
 var AppRouter = Backbone.Router.extend({
         routes: {
             "*actions": "defaultRoute" 
         },
         defaultRoute: function( actions ){
-            event = new Event();
-            view = new EventView({model:event});
+            event = new Event({name:"hello"});
+            event2 = new Event({name:"world"});
+            collection = new EventCollection();
+            collection.add(event).add(event2);
+            view = new RowView({collection:collection});
             $("#container").append(view.render().el);
         }
     });
